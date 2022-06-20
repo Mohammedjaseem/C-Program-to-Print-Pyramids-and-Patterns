b5f02e0c117497175a8dae4c3d3cb9de75d0480d
Mohammed Jaseem Tp <56159302+Mohammedjaseem@users.noreply.github.com>
2022-06-20 21:11:25 +0530

    Update Pattern22.c

--- Fumigation Pattern test/Pattern22.c
+++ Fumigation Pattern test/Pattern22.c
@@ -1,5 +1,5 @@
-/*
 
+/*
 X X
 _ X X
 X X
@@ -9,20 +9,19 @@ X X
 X X
 X X
 _ _ _ X X X X X X X X
-
 */
 
 #include <stdio.h>
     int main(){
         int i,j,k;
-        for(i=1; i<=3; i++){
+        for(i=0; i<=3; i++){
             for(j=0; j<i; j++){
                 printf("X X \n");
             }
             for(k=0; k<i; k++){
                 printf("_ ");
             }
-            for(j=0; j<i; j++){
+            for(j=1; j<=(i*2-2); j++){
                 printf("X X ");
             }
             printf("\n");
