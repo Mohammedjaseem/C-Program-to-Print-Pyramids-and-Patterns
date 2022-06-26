f2ce8f4e4c67461060ccecd3d3601d8ca9d77536
Mohammedjaseem <56159302+Mohammedjaseem@users.noreply.github.com>
2022-06-26 13:26:20 +0530

    Update

---
+++ a1b22.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+    int main(){
+        int i,j,k;
+        for(i=1; i<=7; i++){
+            if(i%2!=0){
+                for(j=0; j<i; j++){
+                printf("%s",65);
+                }
+                printf("\n");
+            }
+
+            else{
+                 for(j=0; j<i; j++){
+                printf("%d",i);
+                }
+                printf("\n");
+            }
+            }
+        }
